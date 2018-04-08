509f37f54cd9f1582a62deb103e685cfb2838942
CristianSmack <cdomingupt@alumnos.unex.es>
2018-04-08 13:16:52 +0200

    Layout Estadisticas

--- app/src/main/java/cristiandevs/apuestasdeportivas/MainActivity.java
+++ app/src/main/java/cristiandevs/apuestasdeportivas/MainActivity.java
@@ -1,12 +1,17 @@
 package cristiandevs.apuestasdeportivas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
+
+import cristiandevs.apuestasdeportivas.UI.EstadisticasActivity;
 
 public class MainActivity extends AppCompatActivity {
     ImageView banner;
@@ -22,7 +27,14 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
+        Button button = findViewById(R.id.estadisticas);
 
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, EstadisticasActivity.class));
+            }
+        });
 
         banner = findViewById(R.id.banner);
 
